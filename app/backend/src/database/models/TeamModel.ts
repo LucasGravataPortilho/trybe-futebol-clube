6f218a2e5a275bfe988af04a7f644826b0ccd3fd
Lucas Portilho <lucasportilho18@gmail.com>
2023-05-10 16:01:52 -0300

    criando model de teams

---
+++ app/backend/src/database/models/TeamModel.ts
@@ -0,0 +1,27 @@
+import { DataTypes, Model } from 'sequelize';
+import db from '.';
+
+class TeamsModel extends Model {
+  declare id: number;
+  declare teamName: string;
+}
+
+TeamsModel.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  team_name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'teams',
+  timestamps: false,
+});
+
+export default TeamsModel;
