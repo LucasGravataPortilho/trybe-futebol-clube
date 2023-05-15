aed5f5db47c6aa179ddea536519f105e0fb77a13
Lucas Portilho <lucasportilho18@gmail.com>
2023-05-15 13:50:50 -0300

    criação dos metodos home de pontuação

--- app/backend/src/database/leaderBoardUtils.ts
+++ app/backend/src/database/leaderBoardUtils.ts
@@ -12,7 +12,48 @@ class LeaderBoard {
 
   constructor(name: string) {
     this.name = name;
+    this.totalPoints = 0;
+    this.totalGames = 0;
+    this.totalVictories = 0;
+    this.totalDraws = 0;
+    this.totalLosses = 0;
+    this.goalsFavor = 0;
+    this.goalsOwn = 0;
+    this.goalsBalance = 0;
+    this.efficiency = 0;
   }
+
+  public restartBoard = () => {
+    this.totalPoints = 0;
+    this.totalGames = 0;
+    this.totalVictories = 0;
+    this.totalDraws = 0;
+    this.totalLosses = 0;
+    this.goalsFavor = 0;
+    this.goalsOwn = 0;
+    this.goalsBalance = 0;
+    this.efficiency = 0;
+  };
+
+  public homeWin = (homeTeamGoals: number, awayTeamGoals: number) => {
+    this.goalsFavor += homeTeamGoals;
+    this.goalsOwn += awayTeamGoals;
+    this.totalPoints += 3;
+    this.totalVictories += 1;
+  };
+
+  public homeLose = (homeTeamGoals: number, awayTeamGoals: number) => {
+    this.goalsFavor += homeTeamGoals;
+    this.goalsOwn += awayTeamGoals;
+    this.totalLosses += 1;
+  };
+
+  public homeTie = (homeTeamGoals: number, awayTeamGoals: number) => {
+    this.goalsFavor += homeTeamGoals;
+    this.goalsOwn += awayTeamGoals;
+    this.totalPoints += 1;
+    this.totalDraws += 1;
+  };
 }
 
 export default LeaderBoard;
